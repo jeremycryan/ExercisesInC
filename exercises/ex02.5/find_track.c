ea64fe418d20e03b37d66d2dc2d32ae9f7b8df63
Jeremy <jeremychristopherryan@gmail.com>
2019-02-14 23:30:12 -0500

    Add regex function

--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -32,12 +32,67 @@ void find_track(char search_for[])
     }
 }
 
-// Finds all tracks that match the given pattern.
+// Finds all tracks that match the given pattern. Takes a string in the form
+// [X-Y], where X is the capital English letter to start with, and Y is the
+// capital English character to end the search.
 //
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
     // TODO: fill this in
+
+    char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    // Determine index of start character
+    int start_index = 0;
+    while (letters[start_index] != pattern[1]) {
+      start_index++;
+      if (start_index >= 26) return;
+    }
+
+    // Determine index of ending character
+    int end_index = 0;
+    while (letters[end_index] != pattern[3]) {
+      end_index++;
+      if (end_index >= 26) {
+        end_index = 25;
+        break;
+      }
+    }
+
+    // Print each song containing that letter
+    int idx = start_index;
+    char a_char[26];
+    int used[NUM_TRACKS];
+    int used_num = 0;
+    int continue_flag = 0;
+
+    // Iterate through characters in range
+    while (idx <= end_index) {
+      a_char[0] = letters[idx];
+      for (int i=0; i<NUM_TRACKS; i++) {
+
+          //  Continue if you have already printed the song this call
+          for (int j = 0; j < used_num; j++) {
+            if (i == used[j]) continue_flag = 1;
+          }
+          if (continue_flag) {
+            continue_flag = 0;
+            continue;
+          }
+
+          // Print songs that match the character
+          if (strstr(tracks[i], a_char)) {
+              printf("Track %i: '%s'\n", i, tracks[i]);
+              used[used_num] = i;
+              used_num++;
+          }
+      }
+
+      idx++;
+    }
+
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -58,8 +113,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    //find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
